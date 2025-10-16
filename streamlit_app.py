e42b7a424249b3192642afdb6707e5ead5dabe65
alykstri <151967940+alykstri@users.noreply.github.com>
2025-10-16 02:09:43 -0400

    checkboxes!

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,14 @@ df = pd.DataFrame({
   'second column': [10, 20, 30, 40]
 })
 
-df
+df #magic display
 
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
