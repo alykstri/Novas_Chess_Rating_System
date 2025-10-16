88a3ffe9f3b537612cd647a337e7e3a3b93af042
alykstri <151967940+alykstri@users.noreply.github.com>
2025-10-16 02:15:18 -0400

    trying options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,14 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
 df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
 
-df #magic display
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
 
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
-
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
-
-    chart_data
-st.line_chart(chart_data)
+'You selected: ', option
+# Options are important
