d9010e78fbe13afe471af85bebec1be2503b93f9
alykstri <151967940+alykstri@users.noreply.github.com>
2025-10-16 02:18:26 -0400

    adding progress bar type widget

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import time
 
 df = pd.DataFrame({
     'first column': [1, 2, 3, 4],
@@ -25,3 +26,18 @@ add_slider = st.sidebar.slider(
     'Select a range of values',
     0.0, 100.0, (25.0, 75.0)
 )
+
+## Progress Bar
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
